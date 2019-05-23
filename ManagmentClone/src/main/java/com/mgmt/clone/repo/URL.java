9da24effd370fcb4c4236885d440f91eac0a3eff
Admin <Admin@DESKTOP-L6RC05T>
2019-05-23 20:59:18 +0530

    First Commit

---
+++ ManagmentClone/src/main/java/com/mgmt/clone/repo/URL.java
@@ -0,0 +1,10 @@
+package com.mgmt.clone.repo;
+
+public class URL {
+
+	public  String getURL()
+	{
+		String url = "jdbc:mysql://localhost:3306/Project_db?user=root&password=root";
+		return url;
+	}
+}
